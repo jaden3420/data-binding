0f9eb7c9e1ad105356b26348196505ac9168589d
jaden3420 <56066097+jaden3420@users.noreply.github.com>
2019-11-26 09:07:13 +0200

    only now done

--- src/exercise1/start/FlagData/FlagRepository.cs
+++ src/exercise1/start/FlagData/FlagRepository.cs
@@ -16,12 +16,10 @@ namespace FlagData
     public class FlagRepository
     {
         public IList<string> Countries { get; }
-
         /// <summary>
         /// The list of available flags
         /// </summary>
         public IList<Flag> Flags { get; }
-
         /// <summary>
         /// Locate a country by the name.
         /// </summary>
@@ -32,7 +30,6 @@ namespace FlagData
             return Flags
                 .FirstOrDefault(f => string.Compare(f.Country, country, StringComparison.OrdinalIgnoreCase) == 0);
         }
-
         /// <summary>
         /// Return the list of countries.
         /// </summary>
@@ -41,22 +38,20 @@ namespace FlagData
         {
             return Flags.Select(f => f.Country);
         }
-
         /// <summary>
         /// Constructor - loads the flags
         /// </summary>
         public FlagRepository()
         {
             var dataAssembly = GetType().GetTypeInfo().Assembly;
-
             // Get our XML data.
             var data = XElement.Load(dataAssembly
                         .GetManifestResourceStream("FlagData.Data.flags.xml"));
-
             // Turn the XML into Flag objects with LINQ to XML.
             var flags = data.Elements("flag")
                 .Select(f =>
-                   new Flag {
+                   new Flag
+                   {
                        Country = f.Attribute("country").Value,
                        ImageUrl = "FlagData." + f.Attribute("imageUrl").Value,
                        DateAdopted = (DateTime)f.Attribute("adopted"),
@@ -64,9 +59,7 @@ namespace FlagData
                        MoreInformationUrl = new Uri(f.Attribute("url").Value),
                        Description = f.Value.Trim()
                    });
-
             Flags = new List<Flag>(flags);
-
             // Read the countries in.
             Countries = new List<string>();
             using (var sr = new StreamReader(dataAssembly.GetManifestResourceStream("FlagData.Data.countries.csv")))
--- src/exercise1/start/FlagFacts/MainPage.xaml.cs
+++ src/exercise1/start/FlagFacts/MainPage.xaml.cs
@@ -3,61 +3,52 @@ using Xamarin.Forms;
 using FlagFacts.Extensions;
 using System;
 using System.Collections;
-
 namespace FlagFacts
 {
     public partial class MainPage : ContentPage
     {
         FlagRepository repository;
         int currentFlag;
-
         public MainPage()
         {
             InitializeComponent();
-
             // Load our data
             repository = new FlagRepository();
-
             // Setup the view
             InitializeData();
         }
-
         public Flag CurrentFlag
         {
-            get {
+            get
+            {
                 return repository.Flags[currentFlag];
             }
         }
-
         private void InitializeData()
         {
-            country.ItemsSource = (IList) repository.Countries;
-            country.SelectedItem = CurrentFlag.Country;
-            country.SelectedIndexChanged += (s, e) => CurrentFlag.Country = repository.Countries[country.SelectedIndex];
-
-            flagImage.Source = CurrentFlag.GetImageSource();
-
-            adopted.Date = CurrentFlag.DateAdopted;
-            adopted.DateSelected += (s, e) => CurrentFlag.DateAdopted = e.NewDate;
-
-            hasShield.IsToggled = CurrentFlag.IncludesShield;
-            hasShield.Toggled += (s, e) => CurrentFlag.IncludesShield = hasShield.IsToggled;
-
-            description.Text = CurrentFlag.Description;
+            country.ItemsSource = (IList)repository.Countries;
+            //  country.SelectedItem = CurrentFlag.Country;
+            // country.SelectedIndexChanged += (s, e) => CurrentFlag.Country = repository.Countries[country.SelectedIndex];
+            country.ItemsSource = (IList)repository.Countries;
+            // country.SelectedItem = CurrentFlag.Country;
+            // country.SelectedIndexChanged += (s, e) => CurrentFlag.Country = repository.Countries[country.SelectedIndex];
+            country.BindingContext = CurrentFlag;
+            country.SetBinding(Picker.SelectedItemProperty,
+                               new Binding(nameof(CurrentFlag.Country)));
+            //flagImage.Source = CurrentFlag.GetImageSource();
+            this.BindingContext = CurrentFlag;
         }
-
         private async void OnShow(object sender, EventArgs e)
         {
+            CurrentFlag.DateAdopted = CurrentFlag.DateAdopted.AddYears(1);
             await DisplayAlert(CurrentFlag.Country,
-                $"{CurrentFlag.DateAdopted:D} - {CurrentFlag.IncludesShield}: {CurrentFlag.MoreInformationUrl}", 
+                $"{CurrentFlag.DateAdopted:D} - {CurrentFlag.IncludesShield}: {CurrentFlag.MoreInformationUrl}",
                 "OK");
         }
-
         private void OnMoreInformation(object sender, EventArgs e)
         {
             Device.OpenUri(CurrentFlag.MoreInformationUrl);
         }
-
         private void OnPrevious(object sender, EventArgs e)
         {
             currentFlag--;
@@ -65,12 +56,11 @@ namespace FlagFacts
                 currentFlag = 0;
             InitializeData();
         }
-
         private void OnNext(object sender, EventArgs e)
         {
             currentFlag++;
             if (currentFlag >= repository.Flags.Count)
-                currentFlag = repository.Flags.Count-1;
+                currentFlag = repository.Flags.Count - 1;
             InitializeData();
         }
     }
